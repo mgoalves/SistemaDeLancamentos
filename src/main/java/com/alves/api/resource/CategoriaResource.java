38292ad981e08adb91124a9287332e1bed29c171
Unknown <marcello_doalves@hotmail.com>
2017-10-10 10:57:23 -0300

    API List - Funcionando.

--- src/main/java/com/alves/api/resource/CategoriaResource.java
+++ src/main/java/com/alves/api/resource/CategoriaResource.java
@@ -1,10 +1,29 @@
 package com.alves.api.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.alves.api.model.Categoria;
+import com.alves.api.repository.CategoriaRepository;
+
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public List<Categoria> list() {
+		
+		return categoriaRepository.findAll();
+	}
+	
+	
 
 }
