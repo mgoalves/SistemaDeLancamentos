003c5d607bcaf8e922e34dd89e581d538674ae11
Unknown <marcello_doalves@hotmail.com>
2017-10-11 08:45:16 -0300

    Salvando categoria

--- src/main/java/com/alves/api/resource/CategoriaResource.java
+++ src/main/java/com/alves/api/resource/CategoriaResource.java
@@ -1,11 +1,18 @@
 package com.alves.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.alves.api.model.Categoria;
 import com.alves.api.repository.CategoriaRepository;
@@ -24,6 +31,18 @@ public class CategoriaResource {
 		return categoriaRepository.findAll();
 	}
 	
+	@PostMapping
+	public ResponseEntity<Categoria> save(@RequestBody Categoria categoria, HttpServletResponse response) {
+		
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+							.buildAndExpand(categoriaSalva.getId()).toUri();
+		
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
 	
 
 }
