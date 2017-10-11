e40c5ad9c88d06fd02767011d1c234b263a909fb
Unknown <marcello_doalves@hotmail.com>
2017-10-11 14:37:57 -0300

    API - Buscar categoria por ID implementada.

--- src/main/java/com/alves/api/resource/CategoriaResource.java
+++ src/main/java/com/alves/api/resource/CategoriaResource.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,5 +45,10 @@ public class CategoriaResource {
 		return ResponseEntity.created(uri).body(categoriaSalva);
 	}
 	
+	@GetMapping({"/{id}"})
+	public Categoria searchById(@PathVariable Long id) {
+		
+		return categoriaRepository.findOne(id);
+	}
 
 }
