766173ca8854267c43548a12e79f499b08d7cb21
Unknown <marcello_doalves@hotmail.com>
2017-10-20 10:44:15 -0200

    Buscar pessoa por Id funcionando.

--- src/main/java/com/alves/api/resource/PessoaResource.java
+++ src/main/java/com/alves/api/resource/PessoaResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,7 +47,7 @@ public class PessoaResource {
 	}
 
 	@GetMapping("/{id}")
-	public Pessoa searchByid(Long id) {
+	public Pessoa searchByid(@PathVariable Long id) {
 		
 		//Retorna pessoa com aquele ID especifico.
 		return pessoaRepository.findOne(id);
