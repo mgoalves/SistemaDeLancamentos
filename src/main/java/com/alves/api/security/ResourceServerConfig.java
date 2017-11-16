79fd28d570a81792f3cc71688227036a07e047d1
Unknown <marcello_doalves@hotmail.com>
2017-11-16 19:46:51 -0200

    O auth2
    Adicionando dependência ao pom e implementando a autorização por meio do acess token.

--- src/main/java/com/alves/api/security/ResourceServerConfig.java
+++ src/main/java/com/alves/api/security/ResourceServerConfig.java
@@ -1,18 +1,22 @@
 package com.alves.api.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Autowired
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 
 		//Tipo de validação seja em memória ou banco 
 		auth.inMemoryAuthentication()
@@ -21,18 +25,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	public void configure(HttpSecurity http) throws Exception {
 	
 		http.authorizeRequests()
 		.antMatchers("/categorias").permitAll() //Acesso sem segurança - exceções
 		.anyRequest().authenticated()			//Acesso com segurança
 		.and()
-		.httpBasic()
-		.and()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //Não mantém estado de sessão
 		.and()
 		.csrf().disable();	//Tratamento JS Injection
 		
+	}
+	
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+
+		resources.stateless(true);
 	}
 	
 }
